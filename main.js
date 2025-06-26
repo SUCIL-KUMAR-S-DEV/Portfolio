744ee4e09e249a5f2500d68cdf4d33d1e89d1fdb
Sucil Kumar S <sucilkumar0709@gmail.com>
2025-06-26 15:30:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+function myFunction(optionGiven) {
+    switch (optionGiven) {
+        case 1:
+            alert("Visting Certifications Page!");
+            window.location.href = "";
+            break;
+            
+        case 2:
+            window.location.href = "https://sucil-kumar-s.github.io/To-Do-List/";
+            break;
+
+        case 3:
+            window.location.href = "https://sucil-kumar-s.github.io/Calculator/";
+            break;
+    
+        default:
+            break;
+    }
+}
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };  
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+};
+ScrollReveal({
+    distance: '80px',
+    duriation: 2000,
+    delay: 200,
+});
+ScroolReveal().reveal('.home-content, heading', { orgin: 'top' });
+ScroolReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { orgin: 'bottom' });
+ScroolReveal().reveal('.home-content h1, .about-img', { orgin: 'left' });
+ScroolReveal().reveal('.home-content p, .about-content', { orgin: 'right' });
